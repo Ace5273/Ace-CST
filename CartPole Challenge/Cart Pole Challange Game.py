617abe19daabd877e20e13a336bfb87c5f224f95
Ace-Code <sagica5273@gmail.com>
2019-05-03 12:36:56 +0300

    yup

--- CartPole Challenge/Cart Pole Challange Game.py
+++ CartPole Challenge/Cart Pole Challange Game.py
@@ -3,6 +3,5 @@ from help_classes.base_game import BaseGame
 class CartPoleChallangeGame(BaseGame):
     def __init__(self):
         super().__init__(width= 600, height= 400)
-    pass
 
 CartPoleChallangeGame().run()
