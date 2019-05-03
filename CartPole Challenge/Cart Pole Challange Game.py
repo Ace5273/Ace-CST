4cb2d1884eb62e10388d3a82a2f59c8adb28a79a
Ace-Code <sagica5273@gmail.com>
2019-05-03 12:03:24 +0300

    Class is now created

---
+++ CartPole Challenge/Cart Pole Challange Game.py
@@ -0,0 +1,6 @@
+from help_classes.base_game import BaseGame
+
+class CartPoleChallangeGame(BaseGame):
+    pass
+
+CartPoleChallangeGame().run()
--- CartPole Challenge/CartPoleGame.py
+++
@@ -1,6 +0,0 @@
-from help_classes.base_game import BaseGame
-
-class CartPoleChallange(BaseGame):
-    pass
-
-CartPoleChallange().run()
