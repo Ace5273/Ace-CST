5ccf24621465a7692c99370b55520eca11be5464
Ace-Code <sagica5273@gmail.com>
2019-05-04 22:35:12 +0300

    Yup

--- CartPole Challenge/Cart Pole Challange Game.py
+++ CartPole Challenge/Cart Pole Challange Game.py
@@ -1,10 +1,11 @@
 from help_classes.base_game import BaseGame
 from help_classes.keyboard import ArrowKeyboard
-from cart import Cart
+from cart import Cart, Pole
 
 class CartPoleChallangeGame(BaseGame):
     def __init__(self):
         super().__init__(width= 600, height= 400)
-        Cart(ArrowKeyboard, 300, 200)
+        Pole(300,200)
+        # Cart(ArrowKeyboard, 300, 200)
 
 CartPoleChallangeGame().run()
--- CartPole Challenge/cart.py
+++ CartPole Challenge/cart.py
@@ -4,13 +4,42 @@ from help_classes.base_game import GameObject
 from help_classes.vector import Point
 from help_classes.keyboard import BaseKeyboard
 
+GRAVITY = 9.8
+
+class Pole(GameObject):
+
+    def __init__(self, x, y):
+        super().__init__(Point(x,y))
+        self.width = 10
+        self.height = 60
+
+    def on_draw(self):
+        arcade.draw_xywh_rectangle_filled(self.position.x - self.width/2, self.position.y, self.width, self.height, arcade.color.BROWN_NOSE)
+        self.draw_position_point(color= arcade.color.RED,size=10)
+    
+    def on_update(self, delta_time):
+
+        # if self.keyboard.is_key_pressed_down('left') ^ self.keyboard.is_key_pressed_down('right'):
+
+        #     if self.keyboard.is_key_pressed_down('left'):
+        #         self.velocity.x = -3
+            
+        #     elif self.keyboard.is_key_pressed_down('right'):
+        #         self.velocity.x = 3
+        
+        # else:
+        #     self.velocity.x = 0
+            
+        super().on_update(delta_time)
+
 class Cart(GameObject):
 
     def __init__(self, keyboard : BaseKeyboard, x, y):
-        super().__init__(Point(x,y))
         self.keyboard = keyboard
         self.width = 70
         self.height = 50
+        # self.pole = Pole(x, y)
+        super().__init__(Point(x,y))
     
     def on_draw(self):
         arcade.draw_xywh_rectangle_filled(self.position.x - self.width/2, self.position.y - self.height/2, self.width, self.height, arcade.color.SILVER)
@@ -18,15 +47,20 @@ class Cart(GameObject):
     
     def on_update(self, delta_time):
 
-        if self.keyboard.is_key_pressed_down('left') ^ self.keyboard.is_key_pressed_down('right'):
+        if (self.keyboard.is_key_pressed_down('left') ^ self.keyboard.is_key_pressed_down('right')):
 
-            if self.keyboard.is_key_pressed_down('left'):
+            if self.keyboard.is_key_pressed_down('left') and (self.position.x - self.width/2) > 0:
                 self.velocity.x = -3
             
-            elif self.keyboard.is_key_pressed_down('right'):
+            elif self.keyboard.is_key_pressed_down('right') and (self.position.x + self.width/2) < 600:
                 self.velocity.x = 3
+
+            else:
+                self.velocity.x = 0
         
         else:
             self.velocity.x = 0
             
         super().on_update(delta_time)
+
+        self.pole.position = self.position
